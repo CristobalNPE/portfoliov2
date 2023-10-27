92a073144ed2edf01eebbc53ef63c44145321965
Cristobal Pulgar <cristobal.npe@gmail.com>
2023-10-27 18:12:30 -0300

    add metadata

--- app/routes/portfolio.tsx
+++ app/routes/portfolio.tsx
@@ -1,11 +1,20 @@
+import type { MetaFunction } from "@remix-run/node";
 import ProjectCard from "~/components/ProjectCard";
 import { Heading } from "~/components/typography/Heading";
 import { data } from "~/data/portfolioData";
 
+export const meta: MetaFunction = () => {
+  return [
+    { title: "Portfolio - Projects" },
+    { name: "description", content: "Portfolio Projects" },
+  ];
+};
+
+
 export default function Portfolio() {
   return (
     <main className="py-5 px-5 md:px-0  mb-[4rem] sm:mb-0">
-      <Heading className="py-7 font-semibold text-center">
+      <Heading className="pt-7 pb-16 font-semibold text-center">
         Some of my projects
       </Heading>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-8 justify-items-center ">
@@ -13,6 +22,7 @@ export default function Portfolio() {
           <ProjectCard key={d.title} {...d} />
         ))}
       </div>
+      
     </main>
   );
 }
--- app/routes/stack.tsx
+++ app/routes/stack.tsx
@@ -1,10 +1,18 @@
+import type { MetaFunction } from "@remix-run/node";
 import StackCard from "~/components/StackCard";
 import { Heading } from "~/components/typography/Heading";
 import { data } from "~/data/stackData";
+export const meta: MetaFunction = () => {
+  return [
+    { title: "Portfolio - Stack" },
+    { name: "description", content: "Technologies Stack" },
+  ];
+};
+
 export default function Stack() {
   return (
     <main className="py-5 px-5 md:px-2  mb-[4rem] sm:mb-0">
-      <Heading className="py-7 text-center font-semibold">
+      <Heading className="pt-7 pb-16 text-center font-semibold">
         Some of technologies I have worked with
       </Heading>
       <div className="flex gap-4 flex-wrap justify-center">
